e160a7addca1cdfe14254d87f8d95390301840a3
Koushik Kumar Vankadari <koushikkumar222@gmail.com>
2017-03-26 19:59:17 +0000

    Added / handler

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const StockController = require("./controllers/stock_controller");
 module.exports = function(app) {
     app.get('/get-stock-data', StockController.availableStocks);
     
-    app.get('/hello', function(req, res, next){
-      res.json({"Helo":"fggbfh"});    
+    app.get('/', function(req, res, next){
+      res.send("server started successfully");    
     });
 };
